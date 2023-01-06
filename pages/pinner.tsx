d23c61632e57187bdea567a177ecb1a71971e336
Samsung <bitup.games@gmail.com>
2023-01-06 15:04:27 +0100

    update: refactor pinner page

--- pages/pinner.tsx
+++ pages/pinner.tsx
@@ -1,71 +1,119 @@
-import { useLazyQuery, useMutation, useQuery } from "@apollo/client"
+import { gql, useApolloClient, useLazyQuery, useMutation, useQuery } from "@apollo/client"
 import { NextPage } from "next/types"
-import { ChangeEvent, useCallback } from "react"
+import { ChangeEvent, useCallback, useState } from "react"
+import { useAccount } from "wagmi"
+import Connect from "../components/Layout/Connect"
 import Spinner from "../components/Layout/Spinner"
-import { CREATE_PIECE } from "../utils/contants"
+import { CREATE_PIECE } from "../utils/constants"
+import { getDate } from "../utils/getDate"
 
-type Props = { websiteID: string }
+const PinnerPage: NextPage = () => {
+  const { address, isConnected } = useAccount()
+  const apolloClient = useApolloClient()
 
-const PinnerPage: NextPage<Props> = ({ websiteID }) => {
+  const [name, setName] = useState("");
+  const [cid, setCid] = useState("");
 
   const [createPiece, { loading: loadingMutation }] = useMutation(CREATE_PIECE)
 
-  const handleSubmit = (event: ChangeEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    const form = event.target
-    const formData = new window.FormData(form)
-    const name = formData.get('name')?.toString()
-    const cid = formData.get('cid')?.toString()
+  const handleOnChangeName = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.value) {
+      setName("")
+      return
+    }
+    setName(e.target.value)
+  }
 
-    form.reset()
+  const handleOnChangeCid = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.value) {
+      setCid("")
+      return
+    }
+    setCid(e.target.value)
+  }
+
+  const handleSubmit = () => {
+    setName("")
+    setCid("")
+    const userEthAccount = apolloClient.readFragment({
+      id: apolloClient.cache.identify({ __typename: "EthAccount", address }),
+      fragment: gql`
+      fragment EthAccountId on EthAccount {
+        id
+      }`
+    })
     createPiece({
       variables: {
         input: {
           content: {
-            websiteID: websiteID,
-            name: name!,
-            cid: cid!,
+            websiteID: process.env.NEXT_PUBLIC_WEBSITE_ID,
+            ownerID: userEthAccount.id,
+            name: name,
+            cid: cid,
             approved: false,
-            rejected: false
+            rejected: false,
+            metadata: {
+              createdAt: getDate(),
+              updatedAt: getDate()
+            }
           }
         }
-      }
+      },
+      // refetchQueries: [
+      //   {
+      //     query: GET_WEBSITE_CONTENT, 
+      //     variables: {
+      //       id: process.env.NEXT_PUBLIC_WEBSITE_ID,
+      //       piecesPageSize: 50,
+      //       subscriptionsPageSize: 50
+      //     }
+      //   },
+      //   'WebsiteContent'
+      // ]
     })
   }
-  
+
   return (
     <div className='px-4 py-10 min-h-screen'>
-      <div className="w-96 border px-6 py-12 m-auto rounded-xl">
+      <div className="w-96 border px-6 pt-8 pb-12 m-auto rounded-xl">
         <p className="text-lg font-semibold mb-6">Pin your item:</p>
-        <form onSubmit={handleSubmit} className='flex flex-col'>
+        <div className='flex flex-col'>
           <p className="text-sm mb-1 ml-1">Name:</p>
-          <input placeholder="Nyan Cat Meme" name="name" type="text" className="form-input text-black rounded-lg mb-4" required/>
+          <input
+            placeholder="Nyan Cat Meme"
+            name="name"
+            type="text"
+            className="form-input text-black rounded-lg mb-4"
+            onChange={handleOnChangeName}
+            value={name}
+          />
           <p className="text-sm mb-1 ml-1">IPFS CID:</p>
-          <input placeholder="bafkreico2zlmcinytg2ri26o4mt73i25ikisd3vth6p4du5gcpek4fsace" name="cid" type="text" className="form-input text-black rounded-lg mb-4" required/>
-          <button
-            className="py-2 px-4 h-10 mt-4 rounded-lg bg-cyan-500 disabled:hover:bg-cyan-500 hover:cursor-point disabled:hover:cursor-default delay-100 hover:bg-cyan-600"
-            type="submit"
-            disabled={loadingMutation}>
-            {loadingMutation ? <Spinner className="h-5 w-5 text-white animate-spin mx-auto" /> : <p>Submit</p>}
-          </button>
-        </form>
+          <input
+            placeholder="bafkreico2zlmcinytg2ri26o4mt73i25ikisd3vth6p4du5gcpek4fsace"
+            name="cid"
+            type="text"
+            className="form-input text-black rounded-lg mb-4"
+            onChange={handleOnChangeCid}
+            value={cid}
+          />
+          {
+            isConnected ? (
+              <button
+                className="py-2 px-4 h-10 mt-4 rounded-lg bg-cyan-500 disabled:hover:bg-cyan-900 disabled:bg-cyan-900 disabled:text-slate-400 hover:cursor-point disabled:hover:cursor-default delay-100 hover:bg-cyan-600"
+                disabled={loadingMutation || !name || !cid}
+                onClick={handleSubmit}
+                >
+                {loadingMutation ? <Spinner className="h-5 w-5 text-white animate-spin mx-auto" /> : <p>Submit</p>}
+              </button>
+            ) : (
+                <Connect className="py-2 px-4 h-10 mt-4 rounded-lg bg-cyan-500 disabled:hover:bg-cyan-500 hover:cursor-point disabled:hover:cursor-default delay-100 hover:bg-cyan-600"/>
+            )
+          }
+        </div>
       </div>
 
     </div>
   )
 }
 
-
-export async function getServerSideProps() {
-
-  if (!process.env.NEXT_PUBLIC_WEBSITE_ID) throw new Error("ENVIROMENT VARIABLE NEXT_PUBLIC_WEBSITE_ID UNDEFINED");
-
-  const websiteID = process.env.NEXT_PUBLIC_WEBSITE_ID
-
-  return {
-    props: {
-      websiteID
-    }
-  }
-}
 export default PinnerPage
