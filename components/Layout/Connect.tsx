bda14b03256db44ba536e8a58ec6b676de09d777
Samsung <bitup.games@gmail.com>
2023-01-06 14:58:06 +0100

    update: handleConnect function

--- components/Layout/Connect.tsx
+++ components/Layout/Connect.tsx
@@ -1,53 +1,50 @@
-import { useApolloClient, useLazyQuery } from "@apollo/client"
-import { useEffect } from "react"
+import { gql, useApolloClient, useQuery } from "@apollo/client"
 import { useConnect } from "wagmi"
 import { MetaMaskConnector } from "wagmi/connectors/metaMask"
-import { CREATE_ETH_ACCOUNT, GET_WEBSITE_USERS } from "../../utils/contants"
+import { CREATE_ETH_ACCOUNT, userPiecesPageSize } from "../../utils/contants"
 import { getDate } from "../../utils/getDate"
 import Spinner from "./Spinner"
 
-export default function Connect() {
+type Props = {
+  className: string;
+}
+export default function Connect({ className }: Props) {
   const { connectAsync, isLoading } = useConnect({
     connector: new MetaMaskConnector(),
   })
-
   const apolloClient = useApolloClient()
 
   const handleConnect = async () => {
     try {
       const { account } = await connectAsync()
-      const { data: websiteData } = await apolloClient.query({
-        query: GET_WEBSITE_USERS,
-        variables: {
-          id: process.env.NEXT_PUBLIC_WEBSITE_ID!,
-          usersPageSize: 1000
-        }
-      })
-      if (websiteData.node?.__typename === "Website") {
-        const userIndex = websiteData.node.users.edges?.findIndex(
-          (edge) => edge && edge.node && edge.node.address === account)
 
-        if (userIndex && userIndex === -1) {
-          await apolloClient.mutate({
-            mutation: CREATE_ETH_ACCOUNT,
-            variables: {
-              input: {
-                content: {
-                  address: account as string,
-                  websiteID: process.env.NEXT_PUBLIC_WEBSITE_ID,
-                  metadata: {
-                    createdAt: getDate(),
-                    updatedAt: getDate()
-                  }
+      const userEthAccount = apolloClient.cache.readFragment({
+        id: apolloClient.cache.identify({ __typename: "EthAccount", address: account }),
+        fragment: gql`
+          fragment UserEthAccountId on EthAccount {
+            id
+          }
+        `
+      })
+
+      if (!userEthAccount) {
+        await apolloClient.mutate({
+          mutation: CREATE_ETH_ACCOUNT,
+          variables: {
+            input: {
+              content: {
+                address: account as string,
+                websiteID: process.env.NEXT_PUBLIC_WEBSITE_ID,
+                metadata: {
+                  createdAt: getDate(),
+                  updatedAt: getDate()
                 }
               }
-            }
-          })
-        } else {
-        }
+            },
+            userPiecesPageSize
+          }
+        })
       }
-
-
     } catch (error) {
 
     }
@@ -55,7 +52,7 @@ export default function Connect() {
 
   return (
     <button
-      className="h-9 min-w-[8rem] px-4 rounded-lg bg-cyan-600 hover:bg-cyan-500 hover:cursor-pointer disabled:cursor-default hover:disabled:bg-cyan-600 "
+      className={className}
       onClick={handleConnect}
       disabled={isLoading}
     >
