65ff6194f53415ee35aca1fd30632764d561aa57
Samsung <bitup.games@gmail.com>
2023-01-11 15:14:12 +0100

    add: tailwind forms plugin

--- next.config.js
+++ next.config.js
@@ -3,5 +3,9 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: [process.env.NEXT_PUBLIC_IPFS_GATEWAY]
-  }
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    // ...
+  ],
 }
--- pages/upload.tsx
+++ pages/upload.tsx
@@ -4,7 +4,7 @@ import { ChangeEvent, useState } from "react"
 import { useAccount } from "wagmi"
 import Connect from "../components/Layout/Connect"
 import Spinner from "../components/Layout/Spinner"
-import { CREATE_PIECE, WebsiteData, websiteDataQueryParams } from "../utils/constants"
+import { CREATE_PIECE, pieceCategories, WebsiteData, websiteDataQueryParams } from "../utils/constants"
 import { getDate } from "../utils/getDate"
 
 const UploadPage: NextPage = () => {
@@ -14,6 +14,7 @@ const UploadPage: NextPage = () => {
 
   const [name, setName] = useState("");
   const [cid, setCid] = useState("");
+  const [category, setCategory] = useState("default");
 
   const [createPiece, { loading: loadingMutation }] = useMutation(CREATE_PIECE, { 
     update: (cache, result) => {
@@ -60,6 +61,10 @@ const UploadPage: NextPage = () => {
     setCid(e.target.value)
   }
 
+  const handleOnChangeCategory = (e: ChangeEvent<HTMLSelectElement>) => {
+    setCategory(e.target.value)
+  }
+  
   const handleSubmit = () => {
     setName("")
     setCid("")
@@ -76,8 +81,9 @@ const UploadPage: NextPage = () => {
           content: {
             websiteID,
             ownerID: userEthAccount.id,
-            name: name,
-            cid: cid,
+            name,
+            cid,
+            category,
             approved: false,
             rejected: false,
             metadata: {
@@ -93,7 +99,7 @@ const UploadPage: NextPage = () => {
   return (
     <div className='px-4 py-10 min-h-screen'>
       <div className="w-96 border px-6 pt-8 pb-12 m-auto rounded-xl">
-        <p className="text-lg font-semibold mb-6">Pin your item:</p>
+        <p className="text-lg font-semibold mb-4 text-center">Pin a new item</p>
         <div className='flex flex-col'>
           <p className="text-sm mb-1 ml-1">Name:</p>
           <input
@@ -113,11 +119,24 @@ const UploadPage: NextPage = () => {
             onChange={handleOnChangeCid}
             value={cid}
           />
+          <p className="text-sm mb-1 ml-1">Category:</p>
+          <select
+            className='text-black rounded-lg mb-4'
+            value={category}
+            onChange={handleOnChangeCategory}
+          >
+            <option disabled hidden value='default'>Select a category</option>
+            {
+              pieceCategories.map((category) => (
+                <option key={category} value={category}>{category}</option>
+              ))
+            }
+          </select>
           {
             isConnected ? (
               <button
                 className="py-2 px-4 h-10 mt-4 rounded-lg bg-cyan-500 disabled:hover:bg-cyan-900 disabled:bg-cyan-900 disabled:text-slate-400 hover:cursor-point disabled:hover:cursor-default delay-100 hover:bg-cyan-600"
-                disabled={loadingMutation || !name || !cid}
+                disabled={loadingMutation || !name || !cid || category === 'default'}
                 onClick={handleSubmit}
                 >
                 {loadingMutation ? <Spinner className="h-5 w-5 text-white animate-spin mx-auto" /> : <p>Submit</p>}
