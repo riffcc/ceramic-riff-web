f6339ff943e8dda7bd078d290b207b0104e00593
Samsung <bitup.games@gmail.com>
2023-01-14 19:41:14 +0100

    fix: change name var  websiteStreamID to siteID

--- components/SubscriptionSearch.tsx
+++ components/SubscriptionSearch.tsx
@@ -5,22 +5,22 @@ import { Website } from "../utils/__generated__/graphql"
 import SubscriptionItem from "./SubscriptionItem"
 
 export default function SubscriptionSearch() {
-  const [websiteStreamID, setWebsiteStreamID] = useState('')
+  const [siteID, setSiteID] = useState('')
   const [showResult, setShowResult] = useState(false)
   const [getWebsite, { loading, data }] = useLazyQuery(GET_WEBSITE)
 
   const openResult = () => setShowResult(true)
   const hideResult = () => setShowResult(false)
-  const onChangeWebsiteStreamID = (e: ChangeEvent<HTMLInputElement>) => {
+  const onChangeSiteID = (e: ChangeEvent<HTMLInputElement>) => {
     hideResult()
     if (!e.target.value) {
-      setWebsiteStreamID('')
+      setSiteID('')
     }
-    setWebsiteStreamID(e.target.value)
+    setSiteID(e.target.value)
   }
 
   const handleOnSearch = async () => {
-    await getWebsite({ variables: { id: websiteStreamID } })
+    await getWebsite({ variables: { id: siteID } })
     openResult()
   }
 
@@ -30,14 +30,14 @@ export default function SubscriptionSearch() {
         <input
           type="text"
           className='form-input w-96 bg-slate-800'
-          placeholder='Wesbite StreamID'
-          value={websiteStreamID}
-          onChange={onChangeWebsiteStreamID}
+          placeholder='Site ID'
+          value={siteID}
+          onChange={onChangeSiteID}
         />
         <button
           className='w-20 h-10 uppercase text-sm font-semibold border border-slate-500 disabled:text-slate-400'
           onClick={handleOnSearch}
-          disabled={websiteStreamID === '' || loading}
+          disabled={siteID === '' || loading}
         >
           Search
         </button>
@@ -48,9 +48,11 @@ export default function SubscriptionSearch() {
           className="absolute inset-x-0 -mt-3 bg-slate-900 border border-slate-500 z-10 w-[29.5rem] mx-auto py-5"
           onMouseLeave={hideResult}
         >
-          {data ?
-            <SubscriptionItem subscription={data.node as Website} /> :
-            <p className="m-auto text-sm">No website found.</p>}
+          {
+            data ?
+              <SubscriptionItem subscription={data.node as Website} /> :
+              <p className="m-auto text-sm">No website found.</p>
+          }
         </div>
       }
     </div>
