08dfa77733cdf1b4950a06fcedc300f6c19b9094
Samsung <bitup.games@gmail.com>
2023-01-30 14:32:46 +0100

    add: date formats

--- utils/getDate.ts
+++ utils/getDate.ts
@@ -1,3 +1,13 @@
-export const getDate = () => {
-  return (new Date).toISOString()
+export const getDate = (f?: 'isows' | 'iso' | 'number') => {
+  const date = new Date
+  if (f === 'iso') {
+    console.log(date.toISOString().substring(0, 16))
+    return date.toISOString().substring(0, 16)
+  } else if (f === 'isows') {
+    return date.toISOString()
+  } else if (f === 'number') {
+    return Date.now()
+  } else {
+    return date.toString()
+  }
 }
