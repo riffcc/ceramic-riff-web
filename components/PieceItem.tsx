d4068e06a180d855259903840339da536776f288
Samsung <bitup.games@gmail.com>
2023-01-30 14:36:13 +0100

    update: styles

--- components/PieceItem.tsx
+++ components/PieceItem.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { HiOutlineThumbDown, HiOutlineThumbUp } from "react-icons/hi";
 import { Piece } from "../utils/__generated__/graphql";
 
 interface Props {
@@ -7,20 +8,22 @@ interface Props {
 }
 export default function PieceItem({ piece }: Props) {
   return (
-    <div className="flex flex-col w-40">
-      <a href={`https://${process.env.NEXT_PUBLIC_IPFS_GATEWAY}/ipfs/${piece.CID}`} target='_blank'>
-        <div className="rounded-xl p-4 h-36 w-36 relative mx-auto bg-gradient-to-b from-slate-700 to-slate-600">
+    <div className="w-40">
+      <div className="rounded-xl p-4 h-36 w-36 relative mx-auto bg-gradient-to-b from-slate-700 to-slate-600">
+        <a href={`https://${process.env.NEXT_PUBLIC_IPFS_GATEWAY}/ipfs/${piece.CID}`} target='_blank'>
           <Image
             alt=""
             src={`https://${process.env.NEXT_PUBLIC_IPFS_GATEWAY}/ipfs/${piece.CID}`}
             fill
             priority
           />
-        </div>
-      </a>
-      <Link href={`/pin/${piece.id}`}>
-        <p className="text-xs mt-2 truncate text-ellipsis text-center">{piece.name}</p>
-      </Link>
+        </a>
+      </div>
+      <div className='w-full h-8'>
+        <Link href={`/pin/${piece.id}`}>
+          <p className="text-xs mt-2 text-center">{piece.name}</p>
+        </Link>
+      </div>
     </div>
   )
 }
