59cd41a0b369fac3f68309b6c59a18c84b269e8c
Samsung <bitup.games@gmail.com>
2023-02-27 20:26:47 +0100

    update: refactor components tree

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,34 +1,29 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ApolloProvider } from '@apollo/client'
-import { useApollo } from '../lib/apolloClient'
 import Layout from '../components/Layout/Layout'
 import { WagmiConfig } from 'wagmi'
 import { client } from '../utils/wagmi'
+import CeramicProvider from '../context/Ceramic'
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const apolloClient = useApollo(pageProps)
-
   return (
     <div>
-      {
-        apolloClient ? (
-          <WagmiConfig client={client}>
-            <ApolloProvider client={apolloClient!}>
-              <Layout>
-                <Component {...pageProps} />
-              </Layout>
-            </ApolloProvider>
-          </WagmiConfig>
-        ) : (
-          <div className="min-h-screen flex flex-col bg-gradient-to-b from-slate-900 to-black text-slate-50">
-          </div>
-        )
-      }
+      <WagmiConfig client={client}>
+        <QueryClientProvider client={queryClient}>
+          <CeramicProvider>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </CeramicProvider>
+        </QueryClientProvider>
+      </WagmiConfig>
     </div>
-
   )
 }
 
 export default MyApp
+
