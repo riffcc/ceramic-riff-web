fbf5c6731cfde58ba3ad86f1ff1d554d003e2c48
Samsung <bitup.games@gmail.com>
2023-02-27 20:10:52 +0100

    add: useMutatePin hook

---
+++ hooks/useMutatePin.tsx
@@ -0,0 +1,40 @@
+import { useMutation } from 'react-query';
+
+type PinProps = {
+  data: {
+    ownerID?: string;
+    websiteID?: string;
+    categoryID?: string;
+    pieceID?: string;
+    approved?: boolean;
+    rejected?: boolean;
+    rejectionReason?: string;
+  };
+  adminID?: string;
+  action: 'create' | 'edit' | 'reject' | 'delete' | 'unreject' | 'approve';
+  pinID?: string;
+};
+
+const useMutatePin = (getPin: (options: any) => void) => {
+  const apiUrl = process.env.NEXT_PUBLIC_ADMIN_SERVER!
+  return useMutation(
+    (params: PinProps) =>
+      fetch(`${apiUrl}/pin`, {
+        method: 'POST',
+        body: JSON.stringify(params),
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+      })
+        .then(response => response.json()),
+    {
+      onSuccess: (data) => {
+        console.log('data from onSuccess in useMutatePin', data)
+        getPin({ variables: { id: data.pinID } });
+      },
+    }
+  );
+};
+
+export default useMutatePin;
